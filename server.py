63adde0f6319888ab6c0fd872a56c2a5a7953660
Aditya Sahu <adityasahu1511@gmail.com>
2025-02-01 15:59:55 -0500

    add db entry in generate_party_code() API

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ import hashlib
 from pydantic import BaseModel, ValidationError, EmailStr
 from flask import Flask, request, jsonify
 
+from pymongo import MongoClient
+client = MongoClient("mongodb://localhost:27017/")
+db = client["brown-backend-db"]
 app = Flask(__name__)
 
 
@@ -21,12 +24,30 @@ def home():
 
 @app.route('/generate_party_code', methods=['POST'])
 def create_party_code():
-
+    party_collection = db["party_collection"]
     try:
         new_party = Create_Party.parse_raw(request.data)
 
     
         party_code = generate_party_code(new_party.username, new_party.email, new_party.party_name)
+
+        print(party_code)
+        existing_party = party_collection.find_one({"party.party_code": party_code})
+        if existing_party:
+            return jsonify({"error": "Party code already exists. Try a different party name."}), 400
+
+
+        party_collection.insert_one({
+            "host": {
+                "username": new_party.username,
+                "email": new_party.email
+            },
+            "party": {
+                "party_code": party_code,
+                "party_name": new_party.party_name,
+                "user_count": 1
+            }
+        })
         return jsonify({'party_code': party_code})
 
     except ValidationError as e:
